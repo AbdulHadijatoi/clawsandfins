43fafe6e554eb6f6c21aca233e8b4cad362cb575
H∆DI <hadi@thiqatech.com>
2023-12-05 14:53:34 +0400

    removed document from popup links

--- resources/views/soft-shelled-mudcrabs/pages/frozen-whole.blade.php
+++ resources/views/soft-shelled-mudcrabs/pages/frozen-whole.blade.php
@@ -4,12 +4,12 @@
         close
     </span>
 </div>
-<!-- <div class="content">
+<div class="content">
     Frozen whole 
 </div>
 <div class="image">
     <img src="{{asset('images/image_3.jpg')}}">
-</div> -->
-<div class="pdf spinner">
-    <iframe onload="$('.spinner').removeClass('spinner')" src="https://docs.google.com/viewer?url=https://clawsandfins.demo.zhardsoft.com/WebRequirements.pdf&embedded=true"></iframe>
 </div>
+{{-- <div class="pdf spinner">
+    <iframe onload="$('.spinner').removeClass('spinner')" src="https://docs.google.com/viewer?url=https://clawsandfins.demo.zhardsoft.com/WebRequirements.pdf&embedded=true"></iframe>
+</div> --}}
